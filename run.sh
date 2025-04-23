a7b46f8f8bc9cc0bdbdc3ce35e7b918c7e98f345
kainoaw <kwoessner@ucsd.edu>
2025-04-23 14:51:38 -0700

    Added run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+# Use ./run.sh to run web app
+
+# Start Flask backend
+cd backend
+source venv/bin/activate
+echo "Starting backend..."
+python app.py &
+cd ..
+
+# Start React frontend
+cd frontend
+echo "Starting frontend..."
+npm run dev
